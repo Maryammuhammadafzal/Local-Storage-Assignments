53d6bc58bf09ca87542e4840ae3fb938ebbaea84
maryammuhammadafzal <natiqnagori7@gmail.com>
2024-09-20 13:18:02 +0500

    new-assignment

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-
+// import 'sweetalert2/src/sweetalert2.scss';
 // Functions
 function submit(){
-var submitBtn = document.getElementById('submit-btn');
-console.log(submitBtn);
+    var submitBtn = document.getElementById('submit-btn');
+    
 
-submitBtn.addEventListener('click' , function(){
-    var userName = document.getElementById('form3Example1c').value;
-    var userEmail = document.getElementById('form3Example3c').value;
-    var userpass = document.getElementById('form3Example4c').value;
-    var repeatPass = document.getElementById('form3Example4cd').value;
-
-   var storedName = localStorage.setItem('userName' , JSON.stringify(userName));
-   var storedEmail = localStorage.setItem('userEmail' , JSON.stringify(userEmail));
-   var storedPassword = sessionStorage.setItem('userPass' , JSON.stringify(userpass));
-   var storedRepeatPassword = sessionStorage.setItem('repeatPass' , JSON.stringify(repeatPass));
-
-     
-})
+    submitBtn.addEventListener('click' , function(){
+        var userName = document.getElementById('form3Example1c').value;
+        var userEmail = document.getElementById('form3Example3c').value;
+        var userpass = document.getElementById('form3Example4c').value;
+        var repeatPass = document.getElementById('form3Example4cd').value;
+        
+        var storedName = localStorage.setItem('userName' , JSON.stringify(userName));
+        var storedEmail = localStorage.setItem('userEmail' , JSON.stringify(userEmail));
+        var storedPassword = sessionStorage.setItem('userPass' , JSON.stringify(userpass));
+        var storedRepeatPassword = sessionStorage.setItem('repeatPass' , JSON.stringify(repeatPass));
+        
+        Swal.fire({
+            template: "#my-template"
+        });
+        
+    })
+    
+    
+    
 }
 
 submit();
+
+
+
+
+   
+   
+   
+      
+    
