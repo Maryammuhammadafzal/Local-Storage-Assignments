31fd5d2d7a44e1db5bdf3d41611dfd81c9dbf5b6
maryammuhammadafzal <natiqnagori7@gmail.com>
2024-09-21 23:03:09 +0500

    new-assignment

--- app.js
+++ app.js
@@ -7,28 +7,32 @@ function submit(){
     submitBtn.addEventListener('click' , function(){
         var userName = document.getElementById('form3Example1c').value;
         var userEmail = document.getElementById('form3Example3c').value;
-        var userpass = document.getElementById('form3Example4c').value;
-        var repeatPass = document.getElementById('form3Example4cd').value;
-        
+       
         var storedName = localStorage.setItem('userName' , JSON.stringify(userName));
         var storedEmail = localStorage.setItem('userEmail' , JSON.stringify(userEmail));
-        var storedPassword = sessionStorage.setItem('userPass' , JSON.stringify(userpass));
-        var storedRepeatPassword = sessionStorage.setItem('repeatPass' , JSON.stringify(repeatPass));
-        
+       
         Swal.fire({
-            template: "#my-template"
+            template: "#my-template",
+            savePasswoed :function (){
+                var saveBtn = document.getElementsByClassName('swal2-confirm swal2-styled');
+               
+                        saveBtn.addEventListener('click' , function(){
+                            var userpass = document.getElementById('form3Example4c').value;
+                            var repeatPass = document.getElementById('form3Example4cd').value;
+                            
+                            var storedPassword = sessionStorage.setItem('userPass' , JSON.stringify(userpass));
+                            var storedRepeatPassword = sessionStorage.setItem('repeatPass' , JSON.stringify(repeatPass));  
+                        })
+                
+            }
+            
         });
         
     })
-    
-    
-    
+     
 }
 
 submit();
-
-
-
 
    
    
